17cd51b04f9a38fcc60a15ea300c54fed7e18695
Li Jye Tong <ljtong1@gmail.com>
2015-11-02 19:23:43 -0800

    Fixed the hat buttons in pet.html

--- PEPPA/www/js/customize.js
+++ PEPPA/www/js/customize.js
@@ -7,17 +7,51 @@ selectheadmenu.onchange=function() {
 	}
 };*/
 
-$("#head_1").on("vclick", function() {
+$(document).delegate("#pet_customize", "pagebeforecreate", function () {        
+
+        var $petPage = $("#pet_customize"),
+            $btnSubmit1 = $("#head_1", $petPage),
+            $btnSubmit2 = $("#head_2", $petPage),
+            $btnSubmit3 = $("#head_3", $petPage);
+			
+
+        $btnSubmit1.off("tap").on("tap", function () {
+		console.log("WHAT THE FUCK IS THIS");
+		$("#head").html("Head 1 Selected");		
+		})
+		
+        $btnSubmit2.off("tap").on("tap", function () {
+		console.log("WHAT THE FUCK IS THIS");
+		$("#head").html("Head 2 Selected");		
+		})
+
+        $btnSubmit3.off("tap").on("tap", function () {
+		console.log("WHAT THE FUCK IS THIS");
+		$("#head").html("Head 3 Selected");		
+		})
+
+});	
+
+$("#head_1").click(function() {
+	console.log("Clicked the first item");
+	alert("First pressed");
 	$("#head").html("Head 1 Selected");
-});
+});	
 
-$("#head_2").on("vclick", function() {
-	$("#head").html("Head 2 Selected");
-});
+// $("#head_1").click(function() {
+	// console.log("Clicked the first item");
+	// $("#head").html("Head 1 Selected");
+// });
 
-$("#head_3").on("vclick", function() {
-	$("#head").html("Head 3 Selected");
-});
+// $("#head_2").on("vclick", function() {
+	// console.log("Clicked the second item");
+	// $("#head").html("Head 2 Selected");
+// });
+
+// $("#head_3").on("vclick", function() {
+	// console.log("Clicked the third item");
+	// $("#head").html("Head 3 Selected");
+// });
 
 /*
 var PePPA = PePPA || {};
