6567b4352a8f2e679de32b0452ff7a60fa22ea28
BakChoy <april.yj.choi@gmail.com>
2015-11-15 19:32:52 -0800

    new task page added; add button doesn't work yet

--- PEPPA/www/js/tasks.js
+++ PEPPA/www/js/tasks.js
@@ -1,37 +1,70 @@
-$(document).ready(function() {
 
-  // Initial loading of tasks
-
-  var i = 0;
-
-  for( i = 0; i < localStorage.length; i++)
-    $("#tasks").append("<li id='task-"+ i +"'>" + localStorage.getItem('task-'+i) +
-                       " <a href='#'>Delete</a></li>");
-
-  // Add a task
-  $("#tasks-list").submit(function() {
-    if ($("#task").val() != "" ) {
-      localStorage.setItem( "task-"+i, $("#task").val() );
-      $("#tasks").append("<li id='task-"+i+"'>"+localStorage.getItem("task-"+i) +
-                         " <a href='#'>Delete</a></li>");
-      $("#task").val("");
-      i++;
+var ntd = {};
+/** Read the new task and add it to the list */
+ntd.add = function(event) {
+    // Read the task from the input
+    var task=$('input').val();
+    if (task) {
+        // Add the task to array and refresh list
+        ntd.list[ntd.list.length] = task;
+        ntd.refresh_list();
+        // Clear the input
+        $('input').val('');
     }
-    return false;
-  });
-
-  // Remove a task
-
-  $("#tasks li a").live("click", function() {
-    localStorage.removeItem($(this).parent().attr("id"));
-    $(this).parent().hide();
-    for(i=0; i&lt;localStorage.length; i++) {
-      if( !localStorage.getItem("task-"+i)) {
-        localStorage.setItem("task-"+i, localStorage.getItem('task-' + (i+1) ) );
-        localStorage.removeItem('task-'+ (i+1) );
-      }
+    event.preventDefault();
+};
+/** Remove the task which was marked as selected */
+ntd.remove = function() {
+    // Remove from array and refresh list
+    ntd.list.splice(ntd.selected,1);
+    ntd.refresh_list();
+};
+/** Recreate the entire list from the available list of tasks */
+ntd.refresh_list = function() {
+    var $tasks = $('#task_list'), i;
+    // Clear the existing task list
+    $tasks.empty();
+    if (ntd.list.length) {
+        // Add the header
+        $tasks.append('<li data-role="list-divider">Task List</li>');
+        for (var i=0;i<ntd.list.length;i++){
+            // Append each task
+            var li = '<li><a data-rel="dialog" data-task="' + i
+                    + '" href="#confirm">' + ntd.list[i] + '</a></li>'
+            $tasks.append(li);
+        }
     }
-  });
+    // Add the header for addition of new tasks
+    $tasks.append('<li data-role="list-divider">Add a task</li>');
+    // Use jQuery Mobile's listview method to refresh
+    $tasks.listview('refresh');
+    // Store back the list
+    localStorage.ntd_list = JSON.stringify(ntd.list || []);
+};
+
+// Initialize the index page
+$(document).delegate('#taskpage','pageinit', function() {
+    // If no list is already present, initialize it
+    if (!localStorage.ntd_list) {
+        localStorage.ntd_list = "[]";
+    }
+    // Load the list by parsing the JSON from localStorage
+    ntd.list = JSON.parse(localStorage.ntd_list);
+    $('#add').bind('vclick', ntd.add);
+    $('li a').live('vclick', function() {
+        ntd.selected = $(this).data('task');
+    });
+    // Refresh the list everytime the page is reloaded
+    $('#taskpage').bind('pagebeforeshow', ntd.refresh_list);
 });
 
-}
+// Bind the 'Done' and 'Not Done' buttons to task removal
+$(document).delegate('#confirm', 'pageinit', function(){
+    $('.remove_task').bind('vclick', ntd.remove);
+});
+
+// Make the transition in reverse for the buttons on the done and notdone pages
+$(document).delegate('#done, #notdone', 'pageinit', function(){
+    // We reverse transition for any button linking to index page
+    $('[href="#taskpage"]').attr('data-direction','reverse');
+})
