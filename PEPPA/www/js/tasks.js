e82aec29c3b0f95e743b233ae70f10e0c7fb85f5
unknown <leehugo1994@gmail.com>
2015-11-27 21:28:29 -0800

    can add task as list item from text input

--- PEPPA/www/js/tasks.js
+++ PEPPA/www/js/tasks.js
@@ -1,37 +1,17 @@
 $(document).ready(function() {
-
-  // Initial loading of tasks
-
-  var i = 0;
-
-  for( i = 0; i < localStorage.length; i++)
-    $("#tasks").append("<li id='task-"+ i +"'>" + localStorage.getItem('task-'+i) +
-                       " <a href='#'>Delete</a></li>");
-
-  // Add a task
-  $("#tasks-list").submit(function() {
-    if ($("#task").val() != "" ) {
-      localStorage.setItem( "task-"+i, $("#task").val() );
-      $("#tasks").append("<li id='task-"+i+"'>"+localStorage.getItem("task-"+i) +
-                         " <a href='#'>Delete</a></li>");
-      $("#task").val("");
-      i++;
-    }
-    return false;
-  });
-
-  // Remove a task
-
-  $("#tasks li a").live("click", function() {
-    localStorage.removeItem($(this).parent().attr("id"));
-    $(this).parent().hide();
-    for(i=0; i&lt;localStorage.length; i++) {
-      if( !localStorage.getItem("task-"+i)) {
-        localStorage.setItem("task-"+i, localStorage.getItem('task-' + (i+1) ) );
-        localStorage.removeItem('task-'+ (i+1) );
-      }
-    }
-  });
+	document.getElementById("newtask").onsubmit = function() {
+	//$("input[type=submit]").click(function(e) {
+		//var tasklist = document.getElementById("tasklist");
+		var input = $("#textinput").val();
+		console.log(input);
+		var newtask = $('<li></li>');
+		//newtask.innerHTML = input;
+		//tasklist.appendChild(newtask);
+		$('#tasklist').append($('<li/>', {
+			'data-role': "list-divider"
+		}).html(input));
+		
+		$('#tasklist').listview('refresh');
+		return false;
+	};
 });
-
-}
