e9846027b90b112caaedbc590e941c096cdf5a8c
unknown <leehugo1994@gmail.com>
2015-11-28 22:45:40 -0800

    localstorage changes for tasks, index page bugged when you go back

--- PEPPA/www/js/tasks.js
+++ PEPPA/www/js/tasks.js
@@ -1,13 +1,45 @@
+var tasknumber = -1;
+var savedtasknumber = -1;
+
+$(document).delegate("#tasks", "pagebeforecreate", function() {
+	var item;
+	for (i = 0; i<window.localStorage.length; i++) {
+		if ($(i).length != 0) {
+			// do nothing
+		} else {
+			item = window.localStorage.getItem(i);
+			$('#tasklist').append($('<li/>', {
+				id: i,
+				'data-role': "list-divider"
+			}).html(item));
+		}
+	}
+	for (i = 0; i<window.localStorage.length; i++) {
+		window.localStorage.removeItem(i);
+	}
+});
 $(document).ready(function() {
 	document.getElementById("newtask").onsubmit = function() {
+		$('#tasklist').listview('refresh');
 	//$("input[type=submit]").click(function(e) {
 		//var tasklist = document.getElementById("tasklist");
+		savedtasknumber = window.localStorage.getItem("savedtasknumber");
+		if (tasknumber != savedtasknumber) {
+			tasknumber = savedtasknumber;
+		}
+		if (tasknumber < 0) {
+			tasknumber = 0;
+		} else {
+			tasknumber++;
+		}
 		var input = $("#textinput").val();
+		window.localStorage.setItem(tasknumber, input);
+		savedtasknumber = tasknumber;
+		window.localStorage.setItem("savedtasknumber", savedtasknumber);
+		console.log(tasknumber);
 		console.log(input);
-		var newtask = $('<li></li>');
-		//newtask.innerHTML = input;
-		//tasklist.appendChild(newtask);
 		$('#tasklist').append($('<li/>', {
+			id: i,
 			'data-role': "list-divider"
 		}).html(input));
 		
