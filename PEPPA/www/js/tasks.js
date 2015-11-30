baf2f551051cf0fbe7f7c44fc4bc9ce4c0cd78d6
unknown <leehugo1994@gmail.com>
2015-11-30 02:48:22 -0800

    added fields to input a date for tasks

--- PEPPA/www/js/tasks.js
+++ PEPPA/www/js/tasks.js
@@ -1,16 +1,17 @@
-var tasknumber = 0;
+var tasknumber;
 $(document).delegate("#taskpage", "pagebeforeshow", function() {
 	console.log("before listview refresh");
 	$('#tasklist').listview('refresh');
 	console.log("after listview refresh");
 	var i;
 	for (i=0; i<localStorage.length; i++) {
-		var item = localStorage.getItem("task-"+i);
-		console.log(item);
-		console.log($("#task-"+i).length);
+		var itemName = localStorage.getItem("task-"+i+"input");
+		var itemTime = localStorage.getItem("task-"+i+"time");
+		//console.log(item+" "+i+" length= "+$("#task-"+i).length);
 		// if item exists in the localStorage and does not already exist in listview, load it onto listview
-		if (item != null && !$("#task-"+i).length) {
-			$('#tasklist').append($('<li/>').attr("id", "task-"+i).append($('<a href="#" data-icon="delete"></a>').html(item)));
+		if (itemName != null && itemTime != null && !$("#task-"+i).length) {
+			$('#tasklist').append($('<li id="task-'+i+'"></li>').append($('<a href="#" data-icon="delete"></a>').append(
+			$('<h2/>').html(itemTime).append($('<p/>').html(itemName)))));
 		}
 	}
 	console.log("before listview refresh");
@@ -25,18 +26,26 @@ $(document).ready(function() {
 	
 	$("#newtask").submit(function() {
 		var input = $("#textinput").val();
+		var month = $("#month option:selected").text();
+		var day = $("#day option:selected").text();
+		var time = month+" "+day;
 		// if text field not empty
 		if (input != "") {
 			// add to localStorage ("task-NUMBER", "TEXT_INPUT")
 			// start from task number = 0
-			if (localStorage.getItem("tasknumber") == null) {
+			if (localStorage.getItem("tasknumber") == null || localStorage.getItem("tasknumber") == 0) {
 				tasknumber = 0;
+				localStorage.setItem("tasknumber", tasknumber);
 			} else {
 				tasknumber = localStorage.getItem("tasknumber");
 			}
 			var taskid = "task-"+tasknumber;
 			console.log("taskid: "+taskid+", input: "+input);
-			localStorage.setItem(taskid, input);
+			console.log("taskid: "+taskid+", time: "+time);
+			
+			
+			localStorage.setItem(taskid+"input", input);
+			localStorage.setItem(taskid+"time", time);
 			
 			// add the task to the listview
 			/*
@@ -58,10 +67,17 @@ $(document).ready(function() {
 	// remove a task and remove it from local storage
 	
 	$("#tasklist li a").live("click", function() {
-		console.log("removing element with id: "+$(this).attr('id'));
-		localStorage.removeItem($(this).attr('id'));
-		$(this).hide();
-		$(this).remove();
+		console.log("removing element with id: "+this.parentNode.parentNode.parentNode.id);
+		localStorage.removeItem(this.parentNode.parentNode.parentNode.id);
+		console.log(this.parentNode.parentNode.parentNode);
+		console.log(this.parentNode.parentNode.parentNode.id);
+		for (i=1; i<=localStorage.length; i++) {
+			if ("task-"+i == this.parentNode.parentNode.parentNode.id) {
+				localStorage.removeItem(i);
+				break;
+			}
+		}
+		$(this).parent().parent().parent().remove();
 		$('#tasklist').listview('refresh');
 	})
 	
